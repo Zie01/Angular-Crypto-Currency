4c989af0b463ec468c152786c5b6da5b1993ac96
Cupcake Zeera <yragrande01@gmail.com>
2018-02-27 23:46:56 +0800

    api calls to allow other component to user api

--- src/app/data.service.ts
+++ src/app/data.service.ts
@@ -1,8 +1,17 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } form '@angular/common/http';
+import 'rxjs/add/operator/map';
 
 @Injectable()
 export class DataService {
 
-  constructor() { }
+	result:any;
+
+  constructor(private_http: HttpClient) { }
+
+  getPrices(){
+  	return this._http.get("https://min-api.cryptocompare.com/data/pricemulti?fsyms=BTC,ETH,IOT&tsyms=USD")
+  	.map(result => this.result = result);
+  }
 
 }
